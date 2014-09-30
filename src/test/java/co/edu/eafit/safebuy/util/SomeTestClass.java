457f34e3be4f8ef2b0f73ede1c73e5060541b061
Sebastian Velez <sebasv89@gmail.com>
2014-09-30 18:19:53 -0500

    Adding a very simple unit test

---
+++ src/test/java/co/edu/eafit/safebuy/util/SomeTestClass.java
@@ -0,0 +1,13 @@
+package co.edu.eafit.safebuy.util;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class SomeTestClass {
+
+	@Test
+	public void myTest(){
+		Assert.assertEquals(1, 1);
+	}
+}
