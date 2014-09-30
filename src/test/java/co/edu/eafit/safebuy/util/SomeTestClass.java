2d338896d49073753cc12780b4ab994f4a63b278
Sebastian Velez <sebasv89@gmail.com>
2014-09-30 18:21:41 -0500

    making the test fail

--- src/test/java/co/edu/eafit/safebuy/util/SomeTestClass.java
+++ src/test/java/co/edu/eafit/safebuy/util/SomeTestClass.java
@@ -4,10 +4,11 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
+
 public class SomeTestClass {
 
 	@Test
 	public void myTest(){
-		Assert.assertEquals(1, 1);
+		Assert.assertEquals(1, 2);
 	}
 }
